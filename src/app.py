93359d91a1fc1b2ff25fc667f7d8ee44a9ee79cf
Titi-13 <cristyrosa25619@gmail.com>
2026-09-10 11:13:00 -0600

    Se agrego estructuta para el Sistema de Diseño de rutas de recoleccion

--- src/app.py
+++ src/app.py
@@ -1,1 +1,11 @@
-print("Hola MUndo dice Titi :)")
+def percibe_funcion(distancias, caracteristica_clientes):
+    #codigo
+    pass
+
+def decide_funcion(Comparacion_rutas, seleccion_eficiente):
+    #codigop
+    pass
+
+def acciona_funcion(ruta_optima):
+    #codigo
+    pass
